77227c647ad36c43aa211797aad5533cf654b5e3
Abhinandan-Sah <abhinandansah222@gmail.com>
2024-01-12 16:31:50 +0530

    code added

---
+++ NodeJS/1st.js
@@ -0,0 +1,5 @@
+var os = require('os');
+console.log("free memory of our system is "+os.freemem);
+console.log("The current home directory is "+os.homedir);
+console.log("OS is release in "+os.release);
+
