4032d2949242a674d03aed52bb841804311fbb14
Abhinandan-Sah <abhinandansah222@gmail.com>
2024-03-15 15:58:48 +0530

    updated

--- UNIT-3 (NodeJS packages)/2-intro-Express.js
+++ UNIT-3 (NodeJS packages)/2-intro-Express.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const app = express();
 
+// app.get('/', function(req, res){
+//     res.send("hello world");
+// })
+const myLogger = function(req, res, next){
+    // res.send("hello world");
+    console.log('LOGGED');
+    next();
+}
+app.use(myLogger);
 app.get('/', function(req, res){
     res.send("hello world");
 })
-
 app.listen(8080);
