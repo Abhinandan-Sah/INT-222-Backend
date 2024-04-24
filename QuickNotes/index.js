ca4ba2680a44bb60c338c875637e41b932b97cfc
Abhinandan-Sah <abhinandansah222@gmail.com>
2024-04-24 19:48:12 +0530

    update

--- QuickNotes/index.js
+++ QuickNotes/index.js
@@ -1,8 +1,7 @@
-console.log(__dirname+'/public')
 const express= require("express");
 const app = express();
 const path = require("path")
-
+const fs= require("fs")
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -10,8 +9,18 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.set('view engine', 'ejs');
 
 app.get("/", (req, res) => {
-    res.render("index.ejs")
+    fs.readdir(`./files`, function(err, files){
+        res.render("index.ejs", {files: files});
+    });
+    
 });
+
+// app.get("/profile/:username", function(req, res){
+//     res.send(req.params.username);
+// })
+// app.get("/profile/:username/:age", function(req, res){
+//     res.send(`Welcome ${req.params.username}, ${req.params.age}`);
+// })
 
 app.listen(3000, (req, res)=>{
     console.log(`Server is working at port ${3000}`);
